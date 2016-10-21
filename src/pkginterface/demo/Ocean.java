f3cebc6879ad379a8d1c0840b1d5528473881b85
dujinqiao <duzi1996@qq.com>
2016-10-21 20:51:11 +0800

    add Ocean.java

---
+++ src/pkginterface/demo/Ocean.java
@@ -0,0 +1,13 @@
+
+package pkginterface.demo;
+
+public class Ocean {
+    public static void main(String[] args){
+        doSwim(new Human("tong"));
+        doSwim(new Submarine("hey"));
+    }
+    
+    static void doSwim(Swimmer swimmer){
+        swimmer.swim();
+    }
+}
